e12ff88a58a730f385db5bb2baf8c6f69997a44e
Lucas Kramer <krame505@umn.edu>
2020-02-24 14:39:12 -0600

    Combine some rules and rename things for clarity

--- grammars/edu.umn.cs.melt.lambdacalc/abstractsyntax/Eval.sv
+++ grammars/edu.umn.cs.melt.lambdacalc/abstractsyntax/Eval.sv
@@ -4,7 +4,7 @@ imports silver:rewrite as s;
 
 -- Rewrite rules from Building Interpreters with Rewriting Strategies (Dolstra and Visser 2002)
 
--- Alpha reduction with explicit substitution (for reference, not used here)
+-- Alpha renaming with explicit substitution (for reference, not used here)
 global alpha::s:Strategy =
   rule on Term of
   | abs(x, e) -> let y::String = freshVar() in abs(y, letT(x, var(y), e)) end
@@ -24,19 +24,11 @@ global eta::s:Strategy =
   end;
 
 -- Let distribution
-global subsVar::s:Strategy =
+global letDist::s:Strategy =
   rule on Term of
   | letT(x, e, var(y)) when x == y -> e
   | letT(x, e, var(y)) -> var(y)
-  end;
-
-global subsApp::s:Strategy =
-  rule on Term of
   | letT(x, e0, app(e1, e2)) -> app(letT(x, e0, e1), letT(x, e0, e2))
-  end;
-
-global subsAbs::s:Strategy =
-  rule on Term of
   | letT(x, e1, abs(y, e2)) when x == y -> abs(x, e2)
   | letT(x, e1, abs(y, e2)) ->
     let z::String = freshVar() in abs(z, letT(x, e1, letT(y, var(z), e2))) end
@@ -44,23 +36,22 @@ global subsAbs::s:Strategy =
 
 -- Full eager evaluation, including reduction inside lambdas
 global evalInnermost::s:Strategy =
-  s:innermost(beta <+ subsVar <+ subsApp <+ subsAbs);
+  s:innermost(beta <+ letDist);
 
 -- Eager evaluation (call by value)
 global evalEager::s:Strategy =
   s:try(traverse app(evalEager, evalEager) <+ traverse letT(_, evalEager, evalEager)) <*
-  s:try((beta <+ subsVar <+ subsApp <+ subsAbs) <* evalEager);
+  s:try((beta <+ letDist) <* evalEager);
 
 -- Lazy evaluation without memoization (call by name)
 global evalLazy::s:Strategy =
   s:try(traverse app(evalLazy, _) <+ traverse letT(_, _, evalLazy)) <*
-  s:try((beta <+ subsVar <+ subsApp <+ subsAbs) <* evalLazy);
+  s:try((beta <+ letDist) <* evalLazy);
 
 -- Fully expand all remaining lets, for clarity
-global expandSubs::s:Strategy =
-  s:innermost(subsVar <+ subsApp <+ subsAbs);
+global elimLets::s:Strategy = s:innermost(letDist);
 
-global eval::s:Strategy = evalInnermost <* expandSubs;
+global eval::s:Strategy = evalInnermost <* elimLets;
 
 -- Helper functions
 function evaluate
